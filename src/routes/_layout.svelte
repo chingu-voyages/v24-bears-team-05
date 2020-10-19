699d870465f9a8070a41d641ac722ef577feb844
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-19 05:14:19 -0700

    comment correction

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,5 @@
 <!-- Layout is for any layout components like Nav. 
-  Similar to layout in Gatsby, present in all pages -->
+  Similar to layout in Gatsby, present on all pages -->
 <script>
   import Nav from "../components/Nav.svelte";
 </script>
