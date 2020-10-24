80874212144b5245041b9432fc42729d8a48654a
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-24 03:18:14 -0700

    fix: these functions are supposed to be async

--- src/routes/brew-guides/route.js
+++ src/routes/brew-guides/route.js
@@ -1,12 +1,12 @@
 const { hookInterface, hookEntityDefinitions } = require("@elderjs/elderjs");
 
 module.exports = {
-  template: 'BrewGuides.svelte',
+  template: "BrewGuides.svelte",
   // the all function returns an array of all of the 'request' objects of a route. Since this is the homepage, there is only one.
-  all: () => [{ slug: "/brew-guides/" }],
+  all: async () => [{ slug: "/brew-guides/" }],
   // the permalink function takes a 'request' object and returns a relative permalink. In this case "/"
   permalink: ({ request }) => request.slug,
-  data: ({ data }) => {
+  data: async ({ data }) => {
     // The data function populates what data should be in available in our Svelte template.
     // Since we will be listing out Elder.js's hooks, we make sure to populate that on the data object so it can be looped through
     // in our Svelte template.
--- src/routes/guide-pour-over/route.js
+++ src/routes/guide-pour-over/route.js
@@ -1,12 +1,12 @@
 const { hookInterface, hookEntityDefinitions } = require("@elderjs/elderjs");
 
 module.exports = {
-  template: 'BrewGuidePourover.svelte',
+  template: "BrewGuidePourover.svelte",
   // the all function returns an array of all of the 'request' objects of a route. Since this is the homepage, there is only one.
-  all: () => [{ slug: "/brew-guides/pour-over-guide/" }],
+  all: async () => [{ slug: "/brew-guides/pour-over-guide/" }],
   // the permalink function takes a 'request' object and returns a relative permalink. In this case "/"
   permalink: ({ request }) => request.slug,
-  data: ({ data }) => {
+  data: async ({ data }) => {
     // The data function populates what data should be in available in our Svelte template.
     // Since we will be listing out Elder.js's hooks, we make sure to populate that on the data object so it can be looped through
     // in our Svelte template.
