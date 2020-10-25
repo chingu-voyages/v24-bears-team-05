54e1e902946c4a61660c087b03e885b7a4d5edf1
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-25 13:13:48 -0700

    fix routes I broke

--- src/routes/brew-guides/route.js
+++ src/routes/brew-guides/route.js
@@ -1,4 +1,5 @@
 module.exports = {
+  template: "BrewGuides",
   all: async () => [{ slug: "/brew-guides/" }],
   permalink: ({ request }) => request.slug,
 };
--- src/routes/guide-chemex/route.js
+++ src/routes/guide-chemex/route.js
@@ -1,4 +1,5 @@
 module.exports = {
+  template: "BrewGuideChemex",
   all: async () => [{ slug: "/brew-guides/chemex-guide/" }],
   permalink: ({ request }) => request.slug,
 };
