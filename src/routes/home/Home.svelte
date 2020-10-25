c7c200fec59b687cf472144443fed570ecf2e66d
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-24 19:03:27 -0700

    remove unnecessary favicon

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -36,9 +36,6 @@
 
 <svelte:head>
   <title>Bear Coffee</title>
-  <link
-    rel="icon"
-    href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='12 0 100 100'><text y='.9em' font-size='90'>☕</text></svg>" />
 </svelte:head>
 
 <main>
