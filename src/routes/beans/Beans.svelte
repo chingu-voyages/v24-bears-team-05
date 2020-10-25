75af4676293114f1f1c337d3357c861d711e6f41
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-24 19:27:48 -0700

    Markup for Beans page

---
+++ src/routes/beans/Beans.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>Bear Coffee Beans</title>
+</svelte:head>
+
+<section>
+  <h1>Choose Your Beans</h1>
+  <hr />
+  <p>
+    Choose from a wide variety of coffee from the top roasters in the US. All
+    coffee is roasted to order and shipped fresh to your door.
+  </p>
+</section>
+<section>
+  <button>Filter</button>
+  <article>
+    <img src="" alt="" />
+    <p />
+  </article>
+</section>
