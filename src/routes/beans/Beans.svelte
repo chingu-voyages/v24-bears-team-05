a064aec758c626363c10c6b295989f42587d99e0
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-24 21:24:31 -0700

    extract to BeanProducts and add margin-top to main

--- src/routes/beans/Beans.svelte
+++ src/routes/beans/Beans.svelte
@@ -1,19 +1,26 @@
+<script>
+  import BeansProducts from "../../components/BeansProducts.svelte";
+</script>
+
+<style>
+  main {
+    margin-top: 3.5rem;
+  }
+</style>
+
 <svelte:head>
   <title>Bear Coffee Beans</title>
 </svelte:head>
 
-<section>
-  <h1>Choose Your Beans</h1>
-  <hr />
-  <p>
-    Choose from a wide variety of coffee from the top roasters in the US. All
-    coffee is roasted to order and shipped fresh to your door.
-  </p>
-</section>
-<section>
-  <button>Filter</button>
-  <article>
-    <img src="" alt="" />
-    <p />
-  </article>
-</section>
+<main>
+  <section>
+    <h1>Choose Your Beans</h1>
+    <hr />
+    <p>
+      Choose from a wide variety of coffee from the top roasters in the US. All
+      coffee is roasted to order and shipped fresh to your door.
+    </p>
+  </section>
+
+  <BeansProducts client-hydrate={{}} />
+</main>
