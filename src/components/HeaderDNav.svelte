f456d4d8cc33089967a01d9b2a96ddd75809710c
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-28 05:14:28 -0700

    border transition

--- src/components/HeaderDNav.svelte
+++ src/components/HeaderDNav.svelte
@@ -21,7 +21,11 @@
       text-decoration: none;
       margin-left: 3rem;
       text-align: center;
+      padding-bottom: 0.77rem;
+      transition: border-color 0.25s;
+      border-bottom: 3px solid transparent;
     }
+    a:hover,
     .curpath {
       padding-bottom: 0.77rem;
       border-bottom: 3px solid #222;
