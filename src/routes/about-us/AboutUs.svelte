87dc2c75c3030c101ac9e53d007369676ff36cc6
rafawelsh <mrrafael1912@gmail.com>
2020-10-29 15:45:19 -0700

    beginning of aboue page

--- src/routes/about-us/AboutUs.svelte
+++ src/routes/about-us/AboutUs.svelte
@@ -8,6 +8,14 @@
 		max-width: 1200px;
 		margin: 0 auto;
 	}
+	h1 {
+		text-align: center;
+	}
+	img {
+		width: 100%;
+		max-width: 900px;
+		max-height: 500px;
+	}
 </style>
 
 <svelte:head>
@@ -15,4 +23,19 @@
 </svelte:head>
 <main>
 	<h1>About Bear Coffee</h1>
+	<hr />
+	<img src="/images/about.jpg" alt="About Bear Coffee - Coffee Beans" />
+	<p>
+		We are a set of small teams that share 1 roasting house dedicated to roast
+		the best coffee.
+	</p>
+	<p>
+		We realized that the coffee market was full of brands that only cared about
+		Instagram aesthetic.
+	</p>
+	<p>
+		Our priority is placed on the coffee our customers buy. We place importance
+		in the method of paying everyone involved in this process a fair, liviable
+		wage.
+	</p>
 </main>
