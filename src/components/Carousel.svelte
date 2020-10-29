05527cce3b3e068e3de11e7d8e32029f8d2e29f4
zxcv3 <jyoo92181@gmail.com>
2020-10-29 19:59:47 +0900

    revised carousel button

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,53 +1,40 @@
 <script>
-// Props
-export let slideIndex = 0;
-export let images = ['./images/carousel/1.jpg', './images/carousel/2.jpg', './images/carousel/3.jpg']
+  // Props
+  export let slideIndex = 0;
+  export let images = [
+    "./images/carousel/1.jpg",
+    "./images/carousel/2.jpg",
+    "./images/carousel/3.jpg",
+  ];
 
-const paginationFactor = 450;
+  const paginationFactor = 450;
 
-$: offset = 0;
+  $: offset = 0;
 
-const showSlides = slideIndex => {
-    offset = slideIndex * paginationFactor
-}
+  const showSlides = (slideIndex) => {
+    offset = slideIndex * paginationFactor;
+  };
 
-const currentSlide = n => {
-    showSlides(slideIndex = n);
-}
-
+  const currentSlide = (n) => {
+    showSlides((slideIndex = n));
+  };
 </script>
 
-<main>
-<div class="items" style="transform: translateX({offset}px);">
-    {#each images as image}
-    <img src="{image}" class="item">
-    {/each}
-</div>
-</main>
-
-
-<div style="text-align:center">
-    <span class="dot" on:click="{() => currentSlide(-0)}"></span> 
-    <span class="dot" on:click="{() => currentSlide(-1)}"></span> 
-    <span class="dot" on:click="{() => currentSlide(-2)}"></span> 
-</div>
-
-
 <style>
-main {
+  main {
     width: 450px;
     overflow: hidden;
     position: relative;
     margin: auto;
-}
+  }
 
-.items {
+  .items {
     display: flex;
     transition: transform 0.4s ease-in-out;
     transform: translateX(0px);
-}
+  }
 
-.item {
+  .item {
     min-width: 450px;
     height: 200px;
 
@@ -61,18 +48,17 @@ main {
     font-size: 10rem;
     user-select: none;
     overflow: hidden;
-}
+  }
 
-.items .item:first-child {
+  .items .item:first-child {
     margin-left: 0;
-}
+  }
 
-.items .item:last-child {
+  .items .item:last-child {
     margin-right: 0;
-}
+  }
 
-
-.dot {
+  .dot {
     cursor: pointer;
     height: 15px;
     width: 15px;
@@ -81,6 +67,30 @@ main {
     border-radius: 50%;
     display: inline-block;
     transition: background-color 0.6s ease;
-}
+  }
 
+  .dotselected {
+    cursor: pointer;
+    height: 15px;
+    width: 15px;
+    margin: 0 2px;
+    background-color: #000000;
+    border-radius: 50%;
+    display: inline-block;
+    transition: background-color 0.6s ease;
+  }
 </style>
+
+<main>
+  <div class="items" style="transform: translateX({offset}px);">
+    {#each images as image}<img src={image} class="item" />{/each}
+  </div>
+</main>
+
+<div style="text-align:center">
+  {#each images as image, i}
+    <span
+      class={slideIndex === -i ? 'dotselected' : 'dot'}
+      on:click={() => currentSlide(-i)} />
+  {/each}
+</div>
