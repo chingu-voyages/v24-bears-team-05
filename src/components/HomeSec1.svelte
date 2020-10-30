5e85be426311d428464e0c3d64ae488e29a71246
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-29 23:22:14 -0700

    desktop dimensions home sec initial

--- src/components/HomeSec1.svelte
+++ src/components/HomeSec1.svelte
@@ -69,11 +69,79 @@
   .img-box img:last-of-type {
     margin-right: 10rem;
   }
+
+  /* Desktop dimensions */
+  .d {
+    display: none;
+  }
+  @media (min-width: 720px) {
+    .m {
+      display: none;
+    }
+    .d {
+      display: unset;
+    }
+    br {
+      display: none;
+    }
+    br.d {
+      display: unset;
+    }
+
+    section {
+      display: grid;
+      grid-template-areas:
+        "title img"
+        "icons img";
+      place-items: center;
+      place-content: center;
+      column-gap: 1rem;
+      padding-right: 1rem;
+    }
+    h2 {
+      grid-area: title;
+      text-align: center;
+      font-size: 3rem;
+      max-width: unset;
+    }
+    .icon-box {
+      grid-area: icons;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+      margin-left: 0;
+      width: 34rem;
+      -moz-column-gap: 1rem;
+      column-gap: 6rem;
+    }
+    img {
+      grid-area: img;
+    }
+    .scale {
+      height: 85%;
+      width: 33rem;
+      max-width: 90%;
+      object-position: 50% 40%;
+    }
+    .icon-box p {
+      font-size: 1.7rem;
+    }
+    .icon {
+      width: 3rem;
+      height: 3rem;
+    }
+  }
 </style>
 
 <section>
   <img class="scale" src="/images/home/plant.jpg" alt="Coffee Plant" />
-  <h2>Only the Best<br />Quality Roasts Pass<br /> Our Test</h2>
+  <h2>
+    Only the Best<br />
+    Quality
+    <br class="d" />Roasts Pass
+    <br />
+    Our Test
+  </h2>
 
   <div class="icon-box">
     <div class="bean">
@@ -82,7 +150,7 @@
     </div>
     <div>
       <img class="icon" src="/images/home/sun_icon.png" alt="Sun Icon" />
-      <p>Small Harvests</p>
+      <p>Small <br class="d" />Harvests</p>
     </div>
     <div>
       <img class="icon" src="/images/home/sprout_icon.png" alt="Sprout Icon" />
@@ -90,11 +158,11 @@
     </div>
     <div>
       <img class="icon" src="/images/home/person_icon.png" alt="Person Icon" />
-      <p>Fair Wages to Farmers</p>
+      <p>Fair Wages <br class="d" />to Farmers</p>
     </div>
   </div>
 
-  <div class="img-box">
+  <div class="img-box m">
     <img src="/images/home/bean_sorting.jpg" alt="Bean sorting" />
     <img src="/images/home/bean_mixing.jpg" alt="Bean mixing" />
   </div>
