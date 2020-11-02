ffb4205ee45bb78ff4b060c3edeede2b863ef375
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-02 12:44:52 -0800

    make login form less basic

--- src/routes/login/Login.svelte
+++ src/routes/login/Login.svelte
@@ -6,8 +6,18 @@
   main {
     margin-top: 3.5rem;
   }
+
+  h2 {
+    text-align: center;
+    margin: 10rem auto 0 auto;
+  }
 </style>
 
+<svelte:head>
+  <title>Bear Coffee Login</title>
+</svelte:head>
+
 <main>
+  <h2>Join us to discover your new obsession<br />☕</h2>
   <LoginForm hydrate-client={{}} />
 </main>
