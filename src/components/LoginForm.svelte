9e29e845c1cc63eb2e4edd784204fed7ede4697b
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-02 09:36:42 -0800

    basic login route and components skeleton

---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,13 @@
+<script>
+  let user;
+  let pass;
+
+  async function submitHandler() {
+    // TODO: Fetch from serverless func that will log the user in.
+  }
+</script>
+
+<form on:submit={submitHandler}>
+  <input type="text" bind:value={user} placeholder="Email" />
+  <input type="password" bind:value={pass} placeholder="Password" />
+</form>
---
+++ src/routes/login/Login.svelte
@@ -0,0 +1,13 @@
+<script>
+  import LoginForm from "../../components/LoginForm.svelte";
+</script>
+
+<style>
+  main {
+    margin-top: 3.5rem;
+  }
+</style>
+
+<main>
+  <LoginForm hydrate-client={{}} />
+</main>
