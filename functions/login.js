283534bf32a6523f47fd78bc804158c6b406bace
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-03 10:03:56 -0800

    finish writing mockup login function

--- functions/login.js
+++ functions/login.js
@@ -2,20 +2,20 @@ var faunadb = require("faunadb"),
   q = faunadb.query;
 
 exports.handler = async (event) => {
+  var { email, password } = JSON.parse(event.body);
+
   var client = new faunadb.Client({ secret: process.env.FAUNA_KEY });
-  let email;
-  let password;
-  let login = await client.query(
+  var login = await client.query(
     q.Login(q.Match(q.Index("Users_by_email"), email), {
       password,
       ttl: q.TimeAdd(q.Now(), 30, "days"),
     })
   );
 
-  return login?.ref
+  return login?.secret
     ? {
         statusCode: 200,
-        body: login.ref,
+        body: login.secret,
       }
     : {
         statusCode: 400,
