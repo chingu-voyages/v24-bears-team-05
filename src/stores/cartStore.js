771fa4b9541ceaebf27bc1695f8432553273ba7f
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-08 05:40:37 -0800

    finalize addToCart API

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,28 +1,43 @@
 import { writable } from "svelte/store";
 
 function createCart() {
-  const { subscribe, set, update } = writable({});
+  const { subscribe, set } = writable([]);
+
+  function setCart(products = []) {
+    // expects an arr of products
+    set(products);
+    localStorage.setItem("cart", JSON.stringify({ products }));
+  }
 
   return {
-    add: (product) =>
-      update(() => {
-        let products = localStorage.getItem("cart");
-        if (products) products = JSON.parse(products);
-        else products = {};
-        let data = { ...products, product };
-        localStorage.setItem("cart", JSON.stringify(data));
-        return data;
-      }),
+    addToCart({ id, name, type, size, price, quantity }) {
+      let data = localStorage.getItem("cart");
+      let products = data ? JSON.parse(data).products : [];
+
+      let productIndex = products.findIndex((v) => v.id == id);
+      if (productIndex != -1 && products[productIndex].size == size) {
+        // product & size exists, update quantity
+        products[productIndex].quantity += quantity;
+      } else {
+        products.unshift({
+          id,
+          name,
+          type,
+          size,
+          price,
+          quantity,
+        });
+      }
+
+      setCart(products);
+    },
     init() {
       // used in Cart to initialize the Store from localStorage onMount, not possible within store
       var data = localStorage.getItem("cart");
-      if (data) set(JSON.parse(data));
-      else {
-        localStorage.setItem("cart", "{}");
-        set({});
-      }
+      if (data) set(JSON.parse(data).products);
+      else this.clear();
     },
-    clear: () => set({}),
+    clear: setCart,
     subscribe,
   };
 }
