e8a8f8f3ad87db69de401a609da5f86f7e04eedc
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-07 19:58:57 -0800

    revise store

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -5,13 +5,16 @@ function createCart() {
 
   return {
     add: (product) =>
-      update((o) => {
-        let data = { ...o, product };
+      update(() => {
+        let products = localStorage.getItem("cart");
+        if (products) products = JSON.parse(products);
+        else products = {};
+        let data = { ...products, product };
         localStorage.setItem("cart", JSON.stringify(data));
         return data;
       }),
     init() {
-      // used in Header to initialize the Store from localStorage onMount, not possible within store
+      // used in Cart to initialize the Store from localStorage onMount, not possible within store
       var data = localStorage.getItem("cart");
       if (data) set(JSON.parse(data));
       else {
