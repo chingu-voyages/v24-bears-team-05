cef3b21c71f8136d5e2735c7eee0bb9cbc243ee0
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-08 06:00:37 -0800

    remove preloading

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -19,7 +19,7 @@
 <div class="container">
   <Header
     hydrate-client={{ permalink: request.permalink }}
-    hydrate-options={{ loading: 'eager', preload: true }} />
+    hydrate-options={{ loading: 'eager' }} />
   {@html templateHtml}
   <Footer />
 </div>
