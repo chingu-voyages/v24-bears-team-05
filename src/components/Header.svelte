47c3e58f9157b79ae53ba3d0d36840b2489d9ed4
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-10 08:03:58 -0800

    delete outdated comment

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -195,13 +195,6 @@
         <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
       </svg>
     </button>
-    <!-- Desktop markup
-    <div>
-      <h2>Beans</h2>
-      <h2>Brew Guides</h2>
-      <h2>About</h2>
-    </div>
-    -->
   </nav>
   <a class="logo" class:hidden={isHome} href="/"> Bear<br />Coffee </a>
   <h6 class="logo" class:hidden={!isHome}>Bear<br />Coffee</h6>
