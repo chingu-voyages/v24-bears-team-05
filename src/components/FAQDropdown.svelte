df04278d21ccad4553bf84ba5d1523f37e415e9b
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-10 14:11:35 -0800

    rm dupe, fix janky transitions w/ svelte

--- src/components/FAQDropdown.svelte
+++ src/components/FAQDropdown.svelte
@@ -1,82 +1,106 @@
 <script>
-	export let faqs;
-	let { coffeeQ, orderingQ } = faqs;
+  import { slide } from "svelte/transition";
 
-	coffeeQ.map((coffee) => (coffee.open = false && coffee));
-	orderingQ.map((ordering) => (ordering.open = false && ordering));
+  export let faqs;
+  let { coffeeQ, orderingQ } = faqs;
+
+  coffeeQ.map((coffee) => (coffee.open = false && coffee));
+  orderingQ.map((ordering) => (ordering.open = false && ordering));
 </script>
 
 <style>
-	main {
-		padding-top: 2em;
-	}
-	hr {
-		margin: 2em auto;
-	}
-	.heading {
-		text-align: center;
-		font-size: 2.5em;
-	}
-	.answer {
-		display: block;
-		opacity: 0;
-		height: 0px;
-		transition: 0.3s ease-out;
-	}
-	.answer.open {
-		background-color: #f9e9e7;
-		padding: 2em 1em;
-		display: block;
-		opacity: 1;
-		height: 100%;
-		transition: 0.5s ease-out;
-	}
-	.question {
-		position: relative;
-		cursor: pointer;
-	}
-	.faq-item > .question::before {
-		content: "✕";
-		position: absolute;
-		right: -10px;
-		top: 0;
-		transform: rotate(45deg);
-		cursor: pointer;
-	}
-	.faq-item > .question.open::before {
-		content: "✕";
-		position: absolute;
-		right: -10px;
-		top: 0;
-		transform: rotate(0deg);
-		cursor: pointer;
-	}
+  main {
+    padding-top: 2em;
+  }
+  hr {
+    margin: 2em auto;
+  }
+  .heading {
+    text-align: center;
+    font-size: 2.5em;
+    margin-bottom: 1.5rem;
+  }
+  .answer {
+    display: block;
+    opacity: 0;
+    height: 0px;
+  }
+  .answer.open {
+    background-color: #f9e9e7;
+    padding: 2em 1em;
+    margin: 1.5rem 0 0 0;
+    display: block;
+    opacity: 1;
+    height: 100%;
+  }
+  .question {
+    position: relative;
+    cursor: pointer;
+  }
+  .faq-item > .question::before {
+    content: "✕";
+    position: absolute;
+    right: -10px;
+    top: 0;
+    transform: rotate(45deg);
+    cursor: pointer;
+  }
+  .faq-item > .question.open::before {
+    content: "✕";
+    position: absolute;
+    right: -10px;
+    top: 0;
+    transform: rotate(0deg);
+    cursor: pointer;
+  }
+  h2 {
+    margin: 0;
+  }
+  .faq-item {
+    margin: 0 0 2rem 0;
+  }
 </style>
 
 <main>
-	<h2 class="heading">Coffee Questions</h2>
-	{#each coffeeQ as { open, ...coffee }, i}
-		<div
-			class="faq-item faq-item{coffee.id}"
-			on:click={() => {
-				coffeeQ[i].open = !coffeeQ[i].open;
-				coffeeQ = [...coffeeQ];
-			}}>
-			<h2 class="question" class:open>{coffee.question}</h2>
-			<p class="answer" class:open>{coffee.answer}</p>
-		</div>
-	{/each}
-	<hr />
-	<h2 class="heading">Shipping Questions</h2>
-	{#each orderingQ as { open, ...ordering }, i}
-		<div
-			class="faq-item faq-item{ordering.id}"
-			on:click={() => {
-				orderingQ[i].open = !orderingQ[i].open;
-				orderingQ = [...orderingQ];
-			}}>
-			<h2 class="question" class:open>{ordering.question}</h2>
-			<p class="answer" class:open>{ordering.answer}</p>
-		</div>
-	{/each}
+  <h2 class="heading">Coffee Questions</h2>
+  {#each coffeeQ as { open, ...coffee }, i}
+    <div
+      class="faq-item faq-item{coffee.id}"
+      on:click={() => {
+        coffeeQ[i].open = !coffeeQ[i].open;
+        coffeeQ = [...coffeeQ];
+      }}>
+      <h2 class="question" class:open>{coffee.question}</h2>
+      {#if open}
+        <p
+          class="answer"
+          class:open
+          in:slide={{ duration: 500 }}
+          out:slide={{ duration: 300 }}>
+          {coffee.answer}
+        </p>
+      {/if}
+    </div>
+  {/each}
+  <hr />
+  <h2 class="heading">Shipping Questions</h2>
+  {#each orderingQ as { open, ...ordering }, i}
+    <div
+      class="faq-item faq-item{ordering.id}"
+      on:click={() => {
+        orderingQ[i].open = !orderingQ[i].open;
+        orderingQ = [...orderingQ];
+      }}>
+      <h2 class="question" class:open>{ordering.question}</h2>
+      {#if open}
+        <p
+          class="answer"
+          class:open
+          in:slide={{ duration: 500 }}
+          out:slide={{ duration: 300 }}>
+          {ordering.answer}
+        </p>
+      {/if}
+    </div>
+  {/each}
 </main>
--- src/routes/faq/route.js
+++ src/routes/faq/route.js
@@ -1,82 +1,76 @@
 module.exports = {
-	template: "FAQ",
-	all: async () => [{ slug: "/faq/" }],
-	permalink: ({ request }) => request.slug,
-	data: async ({ request }) => {
-		const faqs = {
-			orderingQ: [
-				{
-					id: 1,
-					question: "Why was my card charged twice?",
-					answer:
-						"Sometimes there can be an error in processing cards online. This is likely a pending pre-authorization you see on your statement and it should disappear from your credit card transactions within a couple days. Still experiencing an issue? Please feel free to email us at info@bearcoffee.com",
-				},
-				{
-					id: 2,
-					question: "Why is my shipment delayed?",
-					answer:
-						"Couriers can sometimes experience influxes in orders that might overwhelm and skew expected shipment timeframes. If you haven’t already, sign up for updates on your package through your courier by referencing your tracking number.",
-				},
-				{
-					id: 3,
-					question: "Where is my order confirmation?",
-					answer:
-						"Sometimes, our emails end up in people’s junk folders. Other times, the confirmation is sent as a text and is blocked by the phone carrier. Why does this happen? When checking out using PayPal/Amazon or ShopPay they will save your phone number and the notifications are sent to you as a text message. If you have any questions about this, please feel free to please email info@bearcoffee.com providing us with the name on your order. We can then look up your order and provide you with that confirmation.",
-				},
-				{
-					id: 4,
-					question: "Why was my order marked as delivered but it was not?",
-					answer:
-						"Please try checking with your neighbors and your postal delivery person. Also check around your normal delivery place to see if it’s been put somewhere different. Sometimes carriers will mark packages as being delivered but not actually deliver them until the next day. Unfortunately, we do not have any additional information than what you have received. Once we hand off the package to USPS, it’s in their hands to ensure delivery. All that being said, if you are unable to track down your package, please let us know by sending your name and order number to info@bearcoffee.com with a description of the issue you’re experiencing. We want to make sure that you are happy with your purchase from us, and we’ll look into sending a replacement order.",
-				},
-				{
-					id: 5,
-					question: "What do I do if my order arrived damaged?",
-					answer:
-						"We appreciate your business and want to make it right. Please email us at info@bearcoffee.com with your name and order number and a description of the issue you’re experiencing and we will work to make it right.",
-				},
-				{
-					id: 5,
-					question: "What do I do if my order arrived damaged?",
-					answer:
-						"We appreciate your business and want to make it right. Please email us at info@bearcoffee.com with your name and order number and a description of the issue you’re experiencing and we will work to make it right.",
-				},
-			],
-			coffeeQ: [
-				{
-					id: 1,
-					question: "When Do You Roast?",
-					answer:
-						"All of our coffee for the cafes and for online orders are roasted to order, and we roast and ship our coffees Monday through Friday. At most, they are shipped out or delivered to our cafes within 24hrs of being roasted.",
-				},
-				{
-					id: 2,
-					question: "How do I brew my coffee? ",
-					answer:
-						"Curious about how to brew your coffee or use that new brew method? Click here where you can read our  brewing guides. You can also visit each coffee’s product page for videos of how we brewed that specific coffee on filter and espresso.",
-				},
-				{
-					id: 3,
-					question: "What is a Traditional to Modern Scale?",
-					answer:
-						"The Traditional to Modern Scale on our bags refers to the expected flavors in coffee. If the scale is marked more towards Traditional, you can expect flavors like nuts, dry wine, bakers chocolate, etc. With coffees marked more towards Modern, you can expect flavors like citrus, floral, fruit, etc. Typically, but not always, the more modern the coffee, the lighter we usually roast it.",
-				},
-				{
-					id: 4,
-					question: "Are we Fair Trade?",
-					answer:
-						"The short answer is yes and no. Essentially, we meet Fair Trade requirements but we don’t have the actual certifications. We tend to pay 3-4x more than the Fair Trade for our coffees, meaning we go above and beyond what Fair Trade pricing is.",
-				},
-				{
-					id: 5,
-					question: "What is the best way to store my coffee?",
-					answer:
-						"Once one of our bags of coffee is opened, it is good for about 30 days. Our 10 oz boxes are created to be tight and act as coffee containers. Once you open your new coffee for the first time, just roll the bag up and place it back in the box with the lid shut. This will keep your coffee tasting good. Once you have opened the 2lb and 5lb bags, simply folding it over and clamping it shut will keep your coffee fresh. However, for optimal freshness, we recommend an airtight container for these sizes of bags.",
-				},
-			],
-		};
-		return {
-			faqs: faqs,
-		};
-	},
+  template: "FAQ",
+  all: async () => [{ slug: "/faq/" }],
+  permalink: ({ request }) => request.slug,
+  data: async ({ request }) => {
+    const faqs = {
+      orderingQ: [
+        {
+          id: 1,
+          question: "Why was my card charged twice?",
+          answer:
+            "Sometimes there can be an error in processing cards online. This is likely a pending pre-authorization you see on your statement and it should disappear from your credit card transactions within a couple days. Still experiencing an issue? Please feel free to email us at info@bearcoffee.com",
+        },
+        {
+          id: 2,
+          question: "Why is my shipment delayed?",
+          answer:
+            "Couriers can sometimes experience influxes in orders that might overwhelm and skew expected shipment timeframes. If you haven’t already, sign up for updates on your package through your courier by referencing your tracking number.",
+        },
+        {
+          id: 3,
+          question: "Where is my order confirmation?",
+          answer:
+            "Sometimes, our emails end up in people’s junk folders. Other times, the confirmation is sent as a text and is blocked by the phone carrier. Why does this happen? When checking out using PayPal/Amazon or ShopPay they will save your phone number and the notifications are sent to you as a text message. If you have any questions about this, please feel free to please email info@bearcoffee.com providing us with the name on your order. We can then look up your order and provide you with that confirmation.",
+        },
+        {
+          id: 4,
+          question: "Why was my order marked as delivered but it was not?",
+          answer:
+            "Please try checking with your neighbors and your postal delivery person. Also check around your normal delivery place to see if it’s been put somewhere different. Sometimes carriers will mark packages as being delivered but not actually deliver them until the next day. Unfortunately, we do not have any additional information than what you have received. Once we hand off the package to USPS, it’s in their hands to ensure delivery. All that being said, if you are unable to track down your package, please let us know by sending your name and order number to info@bearcoffee.com with a description of the issue you’re experiencing. We want to make sure that you are happy with your purchase from us, and we’ll look into sending a replacement order.",
+        },
+        {
+          id: 5,
+          question: "What do I do if my order arrived damaged?",
+          answer:
+            "We appreciate your business and want to make it right. Please email us at info@bearcoffee.com with your name and order number and a description of the issue you’re experiencing and we will work to make it right.",
+        },
+      ],
+      coffeeQ: [
+        {
+          id: 1,
+          question: "When Do You Roast?",
+          answer:
+            "All of our coffee for the cafes and for online orders are roasted to order, and we roast and ship our coffees Monday through Friday. At most, they are shipped out or delivered to our cafes within 24hrs of being roasted.",
+        },
+        {
+          id: 2,
+          question: "How do I brew my coffee? ",
+          answer:
+            "Curious about how to brew your coffee or use that new brew method? Click here where you can read our  brewing guides. You can also visit each coffee’s product page for videos of how we brewed that specific coffee on filter and espresso.",
+        },
+        {
+          id: 3,
+          question: "What is a Traditional to Modern Scale?",
+          answer:
+            "The Traditional to Modern Scale on our bags refers to the expected flavors in coffee. If the scale is marked more towards Traditional, you can expect flavors like nuts, dry wine, bakers chocolate, etc. With coffees marked more towards Modern, you can expect flavors like citrus, floral, fruit, etc. Typically, but not always, the more modern the coffee, the lighter we usually roast it.",
+        },
+        {
+          id: 4,
+          question: "Are we Fair Trade?",
+          answer:
+            "The short answer is yes and no. Essentially, we meet Fair Trade requirements but we don’t have the actual certifications. We tend to pay 3-4x more than the Fair Trade for our coffees, meaning we go above and beyond what Fair Trade pricing is.",
+        },
+        {
+          id: 5,
+          question: "What is the best way to store my coffee?",
+          answer:
+            "Once one of our bags of coffee is opened, it is good for about 30 days. Our 10 oz boxes are created to be tight and act as coffee containers. Once you open your new coffee for the first time, just roll the bag up and place it back in the box with the lid shut. This will keep your coffee tasting good. Once you have opened the 2lb and 5lb bags, simply folding it over and clamping it shut will keep your coffee fresh. However, for optimal freshness, we recommend an airtight container for these sizes of bags.",
+        },
+      ],
+    };
+    return {
+      faqs: faqs,
+    };
+  },
 };
