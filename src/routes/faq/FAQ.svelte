12fcca8168c28042d463bfdda5757a25a457654e
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-10 13:32:15 -0800

    add all doc titles

--- src/routes/faq/FAQ.svelte
+++ src/routes/faq/FAQ.svelte
@@ -1,29 +1,33 @@
 <script>
-	import FAQDropdown from "../../components/FAQDropdown.svelte";
-	export let data;
-	const { faqs } = data;
+  import FAQDropdown from "../../components/FAQDropdown.svelte";
+  export let data;
+  const { faqs } = data;
 </script>
 
 <style>
-	main {
-		padding: 5em 0em 2em;
-		margin: 0 auto;
-		width: 90vw;
-		max-width: 900px;
-		margin: 0 auto;
-	}
-	h1 {
-		text-align: center;
-	}
+  main {
+    padding: 5em 0em 2em;
+    margin: 0 auto;
+    width: 90vw;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+  h1 {
+    text-align: center;
+  }
 </style>
 
+<svelte:head>
+  <title>Bear Coffee | FAQ</title>
+</svelte:head>
+
 <main>
-	<h1>Frequently Asked Questions</h1>
-	<hr />
-	<p>Questions? Questions? Questions?</p>
-	<p>
-		We got your answers below! If you do not see your answer please email us at
-		info@coffeebear.com
-	</p>
-	<FAQDropdown hydrate-client={{ faqs }} />
+  <h1>Frequently Asked Questions</h1>
+  <hr />
+  <p>Questions? Questions? Questions?</p>
+  <p>
+    We got your answers below! If you do not see your answer please email us at
+    info@coffeebear.com
+  </p>
+  <FAQDropdown hydrate-client={{ faqs }} />
 </main>
