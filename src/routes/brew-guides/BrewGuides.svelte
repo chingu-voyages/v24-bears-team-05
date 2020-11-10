8f560f69deebbd9df6e7c17a850f4ff0dc58a545
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-10 13:27:27 -0800

    set title for brew guides

--- src/routes/brew-guides/BrewGuides.svelte
+++ src/routes/brew-guides/BrewGuides.svelte
@@ -1,121 +1,129 @@
+<script>
+  import { title } from "process";
+</script>
+
 <style>
-	* {
-		box-sizing: border-box;
-	}
-	main {
-		width: 85%;
-		max-width: 1200px;
-		margin: 0 auto;
-		padding: 5em 0em 2em;
-		text-align: center;
-	}
+  * {
+    box-sizing: border-box;
+  }
+  main {
+    width: 85%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 5em 0em 2em;
+    text-align: center;
+  }
 
-	.container {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-	}
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
-	.card {
-		flex: 0 1 350px;
-		max-width: 350px;
-		margin: 2.5em auto;
-		display: grid;
-		grid-template-columns: repeat(6, 1fr);
-		grid-template-rows: repeat(2, 1fr);
-		justify-items: center;
-		position: relative;
-		overflow: hidden;
-		cursor: pointer;
-	}
-	.chemex {
-		background-color: #cf9962;
-	}
-	.aeropress {
-		background-color: #c3cdab;
-	}
-	.pour-over {
-		background-color: #aaa395;
-	}
-	/* text on top of cards */
-	.chemex::after {
-		content: "Chemex";
-		font-size: 3em;
-		position: absolute;
-		top: calc(50% - (17% / 2));
-		left: 0;
-		bottom: 0;
-		right: 0;
-		z-index: 5;
-	}
-	.aeropress::after {
-		content: "Aeropress";
-		font-size: 3em;
-		position: absolute;
-		top: calc(50% - (17% / 2));
-		left: 0;
-		bottom: 0;
-		right: 0;
-		z-index: 5;
-	}
-	.pour-over::after {
-		content: "Pour Over";
-		font-size: 3em;
-		position: absolute;
-		top: calc(50% - (17% / 2));
-		left: 0;
-		bottom: 0;
-		right: 0;
-		z-index: 5;
-	}
+  .card {
+    flex: 0 1 350px;
+    max-width: 350px;
+    margin: 2.5em auto;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    justify-items: center;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+  }
+  .chemex {
+    background-color: #cf9962;
+  }
+  .aeropress {
+    background-color: #c3cdab;
+  }
+  .pour-over {
+    background-color: #aaa395;
+  }
+  /* text on top of cards */
+  .chemex::after {
+    content: "Chemex";
+    font-size: 3em;
+    position: absolute;
+    top: calc(50% - (17% / 2));
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 5;
+  }
+  .aeropress::after {
+    content: "Aeropress";
+    font-size: 3em;
+    position: absolute;
+    top: calc(50% - (17% / 2));
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 5;
+  }
+  .pour-over::after {
+    content: "Pour Over";
+    font-size: 3em;
+    position: absolute;
+    top: calc(50% - (17% / 2));
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 5;
+  }
 
-	.img-box1 {
-		grid-column: 1 / span 4;
-		grid-row: 1;
-		margin: -20px;
-	}
-	.img-box2 {
-		grid-column: 3 / 7;
-		grid-row: 2;
-		margin: -20px;
-	}
+  .img-box1 {
+    grid-column: 1 / span 4;
+    grid-row: 1;
+    margin: -20px;
+  }
+  .img-box2 {
+    grid-column: 3 / 7;
+    grid-row: 2;
+    margin: -20px;
+  }
 
-	img {
-		width: 65%;
-	}
+  img {
+    width: 65%;
+  }
 </style>
 
+<svelte:head>
+  <title>Bear Coffee Brew Guides</title>
+</svelte:head>
+
 <main>
-	<h1>Choose Your Brew</h1>
-	<hr />
-	<h2>
-		Follow along with our experts and brew up a perfect cup of your favorite
-		bean.
-	</h2>
-	<div class="container">
-		<a class="card chemex" href="/brew-guides/aeropress">
-			<div class="image-box img-box1">
-				<img src="/images/brewGuides/chemex1.jpg" alt="" />
-			</div>
-			<div class="image-box img-box2">
-				<img src="/images/brewGuides/chemex2.jpg" alt="" />
-			</div>
-		</a>
-		<a class="card aeropress" href="/brew-guides/aeropress">
-			<div class="image-box img-box1">
-				<img src="/images/brewGuides/chemex1.jpg" alt="" />
-			</div>
-			<div class="image-box img-box2">
-				<img src="/images/brewGuides/chemex2.jpg" alt="" />
-			</div>
-		</a>
-		<a class="card pour-over" href="/brew-guides/pourover">
-			<div class="image-box img-box1">
-				<img src="/images/brewGuides/chemex1.jpg" alt="" />
-			</div>
-			<div class="image-box img-box2">
-				<img src="/images/brewGuides/chemex2.jpg" alt="" />
-			</div>
-		</a>
-	</div>
+  <h1>Choose Your Brew</h1>
+  <hr />
+  <h2>
+    Follow along with our experts and brew up a perfect cup of your favorite
+    bean.
+  </h2>
+  <div class="container">
+    <a class="card chemex" href="/brew-guides/aeropress">
+      <div class="image-box img-box1">
+        <img src="/images/brewGuides/chemex1.jpg" alt="" />
+      </div>
+      <div class="image-box img-box2">
+        <img src="/images/brewGuides/chemex2.jpg" alt="" />
+      </div>
+    </a>
+    <a class="card aeropress" href="/brew-guides/aeropress">
+      <div class="image-box img-box1">
+        <img src="/images/brewGuides/chemex1.jpg" alt="" />
+      </div>
+      <div class="image-box img-box2">
+        <img src="/images/brewGuides/chemex2.jpg" alt="" />
+      </div>
+    </a>
+    <a class="card pour-over" href="/brew-guides/pourover">
+      <div class="image-box img-box1">
+        <img src="/images/brewGuides/chemex1.jpg" alt="" />
+      </div>
+      <div class="image-box img-box2">
+        <img src="/images/brewGuides/chemex2.jpg" alt="" />
+      </div>
+    </a>
+  </div>
 </main>
