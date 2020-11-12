2f88fcbb3924d30d847ea75302aaf8c4d0e4a148
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-11 21:51:54 -0800

    we don't do that here.

--- elder.config.js
+++ elder.config.js
@@ -18,9 +18,6 @@ module.exports = {
     // disable: ['elderWriteHtmlFileToPublic'], // this is used to disable internal hooks. Uncommenting this would disabled writing your files on build.
   },
   plugins: {
-    // '@elderjs/plugin-markdown': { // Markdown plugin, enable if used
-    //   routes: ['blog'],
-    // },
     "@elderjs/plugin-browser-reload": {
       // this reloads your browser when nodemon restarts your server.
       port: 8090,
