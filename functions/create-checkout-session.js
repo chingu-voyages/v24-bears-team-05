ed7f38f8eaecbe49bdfa87cf6fea2b47b9cbb4b1
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-11-14 08:27:06 -0800

    update function to accept email

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -1,7 +1,19 @@
+const { getUser } = require("./utils/fauna");
+
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 exports.handler = async ({ body }) => {
-  const { checkoutItems } = JSON.parse(body);
+  const { checkoutItems, secret } = JSON.parse(body);
+
+  // get user if logged in
+  if (secret) {
+    var email = await getUser(secret);
+  }
+  var opts = {};
+  if (email) {
+    opts = { customer_email: email };
+  }
+
   const { data: prices } = await stripe.prices.list({ limit: 100 });
   const line_items = checkoutItems.map(({ id, size, quantity }) => {
     const priceId = prices.find(
@@ -21,6 +33,7 @@ exports.handler = async ({ body }) => {
     mode: "payment",
     success_url: baseURL + "success",
     cancel_url: baseURL,
+    ...opts, // add email for customer object if logged in
   });
 
   return {
