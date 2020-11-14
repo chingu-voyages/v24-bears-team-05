70effa544c4f86fad867e0fff52eaa46970c4355
rafawelsh <mrrafael1912@gmail.com>
2020-11-14 14:01:34 -0800

    removed duplicate images

--- src/routes/about-us/AboutUs.svelte
+++ src/routes/about-us/AboutUs.svelte
@@ -2,64 +2,64 @@
 </script>
 
 <style>
-  main {
-    padding: 5em 0em 2em;
-    margin: 0 auto;
-    width: 90vw;
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-  .container {
-    max-width: 900px;
-    margin: 0 auto;
-  }
-  h1 {
-    text-align: center;
-    margin: 0;
-  }
-  h3 {
-    font-size: 2em;
-    padding-top: 1em;
-    text-align: center;
-  }
-  p {
-    font-size: 1.25em;
-    margin: 0.5em 0em;
-  }
-  img {
-    padding-top: 1em;
-    margin: 0 auto;
-    width: 100%;
-    max-height: 250px;
-    object-fit: cover;
-  }
+	main {
+		padding: 5em 0em 2em;
+		margin: 0 auto;
+		width: 90vw;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+	.container {
+		max-width: 900px;
+		margin: 0 auto;
+	}
+	h1 {
+		text-align: center;
+		margin: 0;
+	}
+	h3 {
+		font-size: 2em;
+		padding-top: 1em;
+		text-align: center;
+	}
+	p {
+		font-size: 1.25em;
+		margin: 0.5em 0em;
+	}
+	img {
+		padding-top: 1em;
+		margin: 0 auto;
+		width: 100%;
+		max-height: 250px;
+		object-fit: cover;
+	}
 </style>
 
 <svelte:head>
-  <title>About Bear Coffee</title>
+	<title>About Bear Coffee</title>
 </svelte:head>
 <main>
-  <h1>About Bear Coffee</h1>
-  <hr />
-  <div class="container">
-    <img src="/images/about.jpg" alt="About Bear Coffee - Coffee Beans" />
-    <p>
-      We are a set of small teams that share 1 roasting house dedicated to roast
-      the best coffee.
-    </p>
-    <p>
-      We realized that the coffee market was full of brands that only cared
-      about Instagram aesthetic.
-    </p>
-    <p>
-      Our priority is placed on the coffee our customers buy. We place
-      importance in the method of paying everyone involved in this process a
-      fair, liviable wage.
-    </p>
-    <h3>
-      Have some questions?
-      <br />Check out our
-      <a href="/faq">FAQ Page</a>
-    </h3>
-  </div>
+	<h1>About Bear Coffee</h1>
+	<hr />
+	<div class="container">
+		<img src="/images/about.jpg" alt="About Bear Coffee - Coffee Beans" />
+		<p>
+			We are a set of small teams that share one roasting house dedicated to
+			roasting the best coffee.
+		</p>
+		<p>
+			We realized that the coffee market was full of brands that only cared
+			about Instagram aesthetics and not the coffee community.
+		</p>
+		<p>
+			Our priority is placed on the coffee our customers buy. We place
+			importance in the method of paying everyone involved in this process a
+			fair, liviable wage.
+		</p>
+		<h3>
+			Have some questions?
+			<br />Check out our
+			<a href="/faq">FAQ Page</a>
+		</h3>
+	</div>
 </main>
