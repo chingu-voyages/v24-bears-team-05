0e2e403ae49c1fa6a19a233a5a6c7205942137b6
zxcv3 <zxcv34332@gmail.com>
2020-11-15 06:15:10 +0900

    carousel revised

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,19 +1,18 @@
 <script>
   // Props
   export let slideIndex = 0;
-  export let paginationFactor = 1000;
   export let images = [
     "./images/carousel/1.jpg",
     "./images/carousel/2.jpg",
     "./images/carousel/3.jpg",
   ];
-
   $: offset = 0;
 
-  let showSlides = (slideIndex) => {
-    offset = slideIndex * paginationFactor;
-  };
+  let w;
 
+  let showSlides = (slideIndex) => {
+    offset = slideIndex * w;
+  };
   let currentSlide = (n) => {
     showSlides((slideIndex = n));
   };
@@ -22,17 +21,15 @@
 <style>
   .slide-container {
     max-width: 1100px;
-    overflow: hidden;
     margin-left: auto;
     margin-right: auto;
+    overflow: hidden;
   }
-
   .slide {
     display: flex;
     transition: transform 0.4s ease-in-out;
     transform: translateX(0px);
   }
-
   .dot {
     cursor: pointer;
     height: 15px;
@@ -43,7 +40,6 @@
     display: inline-block;
     transition: background-color 0.6s ease;
   }
-
   .dotselected {
     cursor: pointer;
     height: 15px;
@@ -56,10 +52,15 @@
   }
 </style>
 
-<div class="slide-container">
-  <div class="slide" style="transform: translateX({offset}px);">
+<div class="slide-container" bind:clientWidth={w}>
+  <div class="slide" style="transform: translateX({offset}px);;">
     {#each images as image, i}
-      <img src={image} alt="Carousel option {i}" />
+      <div>
+        <img
+          src={image}
+          alt="Carousel option {i}"
+          style="width:{w}px; max-height: 400px; height: 100%;" />
+      </div>
     {/each}
   </div>
 </div>
